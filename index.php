cfb4a972ecf25b9f803a74c121a776e34d2b26c6
aurelien fache <aurelien.fache@fabernovel.com>
2012-10-19 13:57:37 +0200

    add speakers

--- index.php
+++ index.php
@@ -11,6 +11,19 @@
     <link href="css/apidays.css" rel="stylesheet">
     <link rel="icon" href="img/apidays_fav_2.ico">
     <link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:400,200' rel='stylesheet' type='text/css'>
+    <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-35699861-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
   </head>
   <body>
    <div class="container">
