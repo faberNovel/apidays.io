e729695a3503d29c46c176b584233e6130bd1bb8
aurelien fache <aurelien.fache@fabernovel.com>
2012-11-19 18:13:43 +0100

    right texte

--- apidays/index.php
+++ apidays/index.php
@@ -582,7 +582,9 @@ To learn more, please visit our web site and follow us on social networks:</p><a
 						
 						<ul>
 							<li><a href="http://www.squidsolutions.com/" title="Squid Solutions" target="_blank"><img src="images/sponsors/logo_SquidSolutions.png" alt="Squid Solutions" /></a></li>
-							
+							<li><a href="http://fr.7digital.com/" title="7 digital" target="_blank"><img src="images/sponsors/logo_7digital.png" alt="7 digital" /></a></li>
+							<li><a href="http://www.opendatasoft.com/" title="OpenDataSoft" target="_blank"><img src="images/sponsors/logo_OpenDataSoft.png" alt="7 digital" /></a></li>
+							<li><a href="http://www.sendgrid.com" title="SendGrid" target="_blank"><img src="images/sponsors/logo_sendGrid.png" alt="SendGrid" /></a></li>
 						</ul>
 						<div class="clear"></div>
 						
