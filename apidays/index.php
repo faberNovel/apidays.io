620f441d74db740fc429fa557463318b9f1fb3d0
aurelien fache <aurelien.fache@fabernovel.com>
2012-11-26 14:29:02 +0100

    add keynote title for vouillon

--- apidays/index.php
+++ apidays/index.php
@@ -706,7 +706,7 @@ To learn more, please visit our web site and follow us on social networks:</p><a
 							<li><a href="http://fr.7digital.com/" title="7 digital" target="_blank"><img src="images/sponsors/logo_7digital.png" alt="7 digital" /></a></li>
 							<li><a href="http://www.opendatasoft.com/" title="OpenDataSoft" target="_blank"><img src="images/sponsors/logo_OpenDataSoft.png" alt="7 digital" /></a></li>
 							<li><a href="http://www.sendgrid.com" title="SendGrid" target="_blank"><img src="images/sponsors/logo_sendGrid.png" alt="SendGrid" /></a></li>
-							<li><a href="http://www.leetchi.com/" title="Leetchi" target="_blank"><img src="images/sponsors/logo_Leetchi.png" alt="Leetchi" /></a></li>
+							<li><a href="http://www.leetchi-api.com/" title="Leetchi" target="_blank"><img src="images/sponsors/logo_Leetchi.png" alt="Leetchi" /></a></li>
 							<li><a href="http://www.mailjet.com/" title="mailjet" target="_blank"><img src="images/sponsors/logo_MailJet.png" alt="mailjet" /></a></li>
 							
 						</ul>
