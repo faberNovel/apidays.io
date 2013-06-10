9db30260bacabb2ad86697cb0cb7b6d2c41b69a4
mathemagie <aurelien.fache@gmail.com>
2013-06-10 16:48:19 +0200

    change repo

---
+++ tpl_apidays/paris/2013/index.php
@@ -0,0 +1,158 @@
+<?php 
+if ($_GET['y'] != '2013') {
+	//header('Location: http://apidays.io');
+	//exit();
+}
+?>
+<!doctype html>
+<!--[if IE 6]><html lang="en" class="no-js ie6"><![endif]-->
+<!--[if (gt IE 6)|!(IE)]><!--><html lang="en" class="no-js"><!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<title>#APIdays back in 2013</title>
+	<meta name="description" content="Apidays">
+	<meta name="author" content="">
+	<meta name="viewport" content="user-scalable=yes, width=device-width" />
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="keywords" content="API 2013 apidays" />
+	
+	<meta name="copyright" content="Apidays" />
+      <meta name="robots" content = "all" />
+      <meta name="robots" content = "index, follow" />
+      <meta name="revisit-after" content="30 days" />
+      <meta name="audience" content="all" />
+	<!-- favicon 16x16 -->
+	<link rel="shortcut icon" href="/apidays_fav.ico">
+	<!-- apple touch icon 57x57 -->
+	<link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:400,700,300,200' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/styles.css?v=1.0">
+	<link rel="stylesheet" href="css/2013.css?v=1.0">
+	<link href="../css/fonz.css" type="text/css" rel="stylesheet"></style>
+
+	<!-- Remove the script reference below if you're using Modernizr -->
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<?php 
+	$url = 'http://apidays.io/paris/2013/';
+?>
+<body>
+	<div id="wrapper">
+		<div class="main_container">
+			<section class="section home" id="home">
+				<div class="inside fil">
+				
+					<div class="sleft">
+						<img src="images/logo_apiday.png" alt="Api Day" class="logo" />
+						
+						<div class="socials">
+							<br/>
+							<a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php echo $url;?>" data-via="apidays2012" data-related="apidays2012">Tweet</a>
+	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+							<br/><br/>
+							<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+<script type="IN/Share" data-url="<?php echo $url;?>" data-counter="right"></script>
+							<br/><br/>
+							<div class="fb-like" data-send="false" data-href="<?php echo $url;?>" data-layout="button_count" data-width="450" data-show-faces="false"></div>
+
+						</div><!-- fin socials -->
+						<div class='poweredby'>
+								<!--<br/><br/><img src="images/powered_by.png" alt="powered by" />-->
+						</div>
+						
+					</div><!-- fin sleft -->
+					
+					<div class="main">
+						<div class="pad">
+							<h1>The Web 1.0 was readable, the Web 2.0 was social, <br />
+							<span class="be">now the web is programmable</span>
+							<span class="te">trough <span class="colr">a</span>pplication <span class="colr">p</span>rogramming <span class="colr">i</span>nterfaces,</span>
+							<span class="ne">aka APIs.</span></h1>
+						</div><!-- fin content -->
+						
+						<div class="hr"></div>
+						<div class="pad groupeButton">
+							<a target='_blank' href="http://fabernovel.us1.list-manage.com/subscribe/?u=007196e31b139575078fd3780&amp;id=439d4b3d82" alt="Register" class="button register">Get in touch for next editions</a>
+							<a target='_blank' href="http://youtube.com/apidays?s" alt="videos APIdays 2012 " class="button videos">APidays 2012 in videos</a>
+
+							<div class="clear"></div>
+							
+						</div><!-- fin pad -->
+						
+						<div class="hr"></div>
+						
+						<div class="pad">
+							<p>Web APIs are a unique opprtunity for companies, governments and developers to either better organize the governance of their IT into a scalable  and flexible model, either leverage a complete ecosystem around the organization, exchanging data through the web for trillion of applications on billion devices.</p>
+							
+							<p class="p2">Because each civilization has been described by the way it communicates and make exchanges, API are the beginning of a programmable civilization.</p>
+						</div><!-- fin pad -->
+					
+					</div><!-- fin main -->
+					<div class="clear"></div>
+				</div><!-- fin inside -->
+			</section><!-- fin section -->
+	</div><!-- fin main_container-->
+
+
+		<footer class="footer">
+			<div class="mentions">WE ARE LOOKING FORWARD TO MEET YOU ! &nbsp;&nbsp;&nbsp;<span>APIdays is an event brought to you by fabernovel &amp; webshell</span></div>
+		</footer><!-- fin footer -->
+	
+	</div><!-- fin wrapper -->
+	
+	<div class="iconMove">
+		<img src="images/AD_icon0.png" alt="icon" id="icon0" /> 
+		<img src="images/AD_icon1.png" alt="icon" id="icon1" /> 
+		<img src="images/AD_icon2.png" alt="icon" id="icon2" /> 
+		<img src="images/AD_icon3.png" alt="icon" id="icon3" /> 
+		<img src="images/AD_icon4.png" alt="icon" id="icon4" /> 
+		<img src="images/AD_icon5.png" alt="icon" id="icon5" /> 
+		<img src="images/AD_icon6.png" alt="icon" id="icon6" /> 
+		<img src="images/AD_icon7.png" alt="icon" id="icon7" /> 
+		
+		<img src="images/AD_icon0.png" alt="icon" id="icon8" /> 
+		<img src="images/AD_icon1.png" alt="icon" id="icon9" /> 
+		<img src="images/AD_icon2.png" alt="icon" id="icon10" /> 
+		<img src="images/AD_icon3.png" alt="icon" id="icon11" /> 
+		<img src="images/AD_icon4.png" alt="icon" id="icon12" /> 
+		<img src="images/AD_icon5.png" alt="icon" id="icon13" /> 
+		<img src="images/AD_icon6.png" alt="icon" id="icon14" /> 
+		<img src="images/AD_icon7.png" alt="icon" id="icon15" />
+	</div><!-- fin inconMove -->
+	
+	
+<script src="js/jquery.js"></script>
+<script src="js/jquery/jquery.animate-colors-min.js"></script>
+
+<script src="js/jquery/jquery.scrollTo.js"></script>
+<script src="js/jquery/jquery.nav.js"></script>
+<!-- BG JS -->
+<script src="js/lib/paper.min.js" type="text/javascript" id="paperJs"></script>
+<script src="js/lib/bg.min.js" type="text/paperscript" canvas="draw" id="bgJs"></script>
+<script src="js/app.js"></script>
+<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=223599037652849";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+	</script>
+
+  <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-35699861-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</body>
+</html>
