861bd9b5b68735e04cc4e1c156626dad5543131b
mathemagie <aurelien.fache@gmail.com>
2013-06-19 16:54:36 +0200

    add paris 2013 landing page

---
+++ paris_apidays_2013/index.php
@@ -0,0 +1,266 @@
+<?php 
+	error_reporting(E_ALL ^ E_NOTICE);
+	ini_set('display_errors', '1');
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>API days, The First International event on APIs in Europe #apidays</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/apidays.css" rel="stylesheet">
+    <link rel="icon" href="img/apidays_fav_2.ico">
+    <link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:400,200' rel='stylesheet' type='text/css'>
+    <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-35699861-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+  </head>
+  <body>
+   <div class="container">
+	   	<div class="row-fluid bloc_share">
+	   		<div class="span2">
+		   		<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+<script type="IN/Share" data-counter="right"></script>
+			</div>
+	    	<div class="span1">
+		    	<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://apidays.io" data-via="apidays2012" data-related="apidays2012">Tweet</a>
+	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+			</div>
+
+	    	<div class="span9">
+	    		<div class="fb-like" data-href="http://apidays.io" data-send="true" data-width="450" data-show-faces="false" data-action="recommend" data-font="arial"></div>
+	    	</div>
+		</div>
+		<br/>
+	 	<div class="row-fluid">
+	      	<div class="span4"><img src="img/logo.png" ></div>
+				<div class="span8 title">
+					The	Web	1.0	was	readable, the Web 2.0 was social, <span class='text_blue'><strong>now the web is programmable;</strong>through application programming interfaces, aka APIs.</span><br/><br/>
+					<span class="text1">
+						Web	 APIs are a unique opportunity for companies, governments and	
+						developers to both better organize the governance of their IT into a scalable	
+						and	 flexible model, either leverage a complete ecosystem around the	
+						organization, exchanging data through the web for trillion of applications on billion of devices.
+					</span><br/><br/>
+					Because	each civilization has been	described by the way it	communicates and	
+					makes exchanges,	API	are	the	beginning of a programmable	civilization.
+				</div>
+	  	  	</div>
+	  	  	<br/><br/>
+		  	<div class="row-fluid bloc_date">
+				<div class="span7 date_lieu_event">
+					
+						DECEMBER 3, 2012 > DECEMBER 4, 2012 - Paris, @EPITA Porte d'Italie<br>
+						<span class="textbig">The First	International event	on APIs	in Europe</span>
+					
+				</div>
+				<div class="span5">
+					<?php if (1) {?>
+						<div class="container_register">
+							 <a target='_blank' href="http://www.amiando.com/apidays.html" class="register btn btn-large btn-info">REGISTER NOW</a>
+						</div>
+
+
+					<?php } else {?>
+					<form action="http://fabernovel.us1.list-manage.com/subscribe/?u=007196e31b139575078fd3780&amp;id=439d4b3d82" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+			    		<div class="input-prepend form_email">
+				      		<input type="email" value="" name="EMAIL" id="mce-EMAIL" placeholder="Your email" required>
+					      	 <button action="submit" class="btn btn-large btn-info">Stay tuned</button>
+				      	 </div>
+				     </form>
+				     <?php } ?>
+				</div>
+		    </div>
+		    <div class="bloc_press">
+		   	 <div class="row-fluid">
+		   	 	<div class="span7">
+		   	 		<a href="https://twitter.com/apidays2012" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @apidays2012</a>
+					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		   	 	</div>
+		    	<div class="span5 text_press">PRESS &amp; PARTNERSHIP : <span class="text_blue">contact@apidays.io</span></div>
+		    </div>
+			</div>
+			<?php if (1) {?>
+<!--			<img src="http://maps.googleapis.com/maps/api/staticmap?center=paris&zoom=13&size=600x300&sensor=false"/>-->
+			<a name="orgnizer"></a>
+			<div class="row-fluid">
+				<div class="span2 title_section">
+					ORGANIZERS
+				</div>
+				<div class="span10">
+				</div>
+		    </div>
+		    <br/>
+		   	<div class="row-fluid">
+				<div class="span4">
+					<a target='_blank' href='http://fabernovel.com/en/'><img src="http://www.fabernovel.com/images/images/faberlogo_SVG.svg" class="img-rounded"></a>
+				</div>
+				<div class="span8">
+					<?php 
+						$text = 'startups';
+						if (isset($_GET['spider'])) $text = 'API';
+					?>
+					<a target='_blank' href="http://fabernovel.com/en/">faberNovel</a> helps large organizations to think and act like <?php echo $text;?>. faberNovel combines technology,design and emerging trends to invent new products, services, and experiences. 
+Founded in 2003, faberNovel is based in Paris, San Francisco, New York and Moscow, has a team of 60 passionate people and works for large organizations such as : L'Or&eacute;al, Suez Environnement, Toys "R" Us, Danone, Essilor, bioM&eacute;rieux, SNCF, France TV.
+				</div>
+		    </div>
+		  	 <br/>
+		      <div class="row-fluid">
+				<div class="span4">
+					<a target='_blank' href='http://webshell.io'><img src="http://webshell.io/images/webshell-logo-rc1.png" class="img-rounded"></a>
+				</div>
+				<div class="span8">
+					<a target='_blank' href="http://webshell.io">Webshell</a> enables developers to make advanced web and mobile applications based on APIs easily.
+					Webshell has developped a shell for the web that developers can use to combine APIs of the programmable web in minutes and really program the web.
+					On the Webshell scripting interface, developers combine efficiently both server-side and client side APIs in a console based Javascripty interface,  with authentication and auhtorization by Oauth1.0 or Oauth2.0 in one line of code.
+					Webshell was created in 2012 by its team of 3 co-founders , in Paris, and manages the <a target='_blank' href="http://api500.com/">API rating agency blog</a> and the <a target='_blank' href="http://apijoy.tumblr.com/">{"apis":"the joy"} blog</a>.
+				</div>
+		    </div>
+
+		   
+		   	<a name="official_partner"></a>
+		    <div class="row-fluid">
+				<div class="span3 title_section">
+					OFFICIAL PARTNER
+				</div>
+				<div class="span9">
+				</div>
+		    </div>
+		    <br/>
+		    <div class="row-fluid">
+				<div class="span3">
+					<a target='_blank' href="http://www.epita.fr/"><img src="img/sponsors/Epita.jpg" class="img-rounded"></a>
+				</div>
+				<div class="span9">
+					Established since 28 years, <a target='_blank' href="http://www.epita.fr/">EPITA</a> is a graduate school of computer science for those who design, develop and improve Information and Communication Technology (ICT). The pedagogy of the school is to provide students with the fundamentals of engineering and high competency in the fields of computer science and ICT. The Web Shell platform is the result of final year projects developed by the students as part of the major Multimedia and Information Technology (MIT).
+				</div>
+		    </div>
+		    <br/>
+		       	<a name="official_sponsor"></a>
+		    <div class="row-fluid">
+				<div class="span3 title_section">
+					OFFICIAL SPONSOR
+				</div>
+				<div class="span9">
+				</div>
+		    </div>
+		    <br/>
+		    <div class="row-fluid">
+				<div class="span3">
+					<a target='_blank' href="http://www.layer7tech.com/"><img width="220px" height="300px" src="img/sponsors/layer7.png" class="img-rounded"></a>
+				</div>
+				<div class="span9">
+					Layer 7 Technologies helps enterprises and services providers manage APIs in the cloud; across the Internet; and out to mobile devices. The Layer 7 API Management suite provides the ability to address API security, version management, SLA enforcement, visibility and developer on-boarding requirements, in a fully-integrated suite. Enterprises and service providers can expose APIs externally or internally in a secure, reliable and manageable way, turning the enterprise into and extensible platform.  Deliverable on-premise or from the Cloud, the Layer 7 API Management suite is certified to support the most rigorous security needs including PCI, FIPS, STIG and OAuth.
+To learn more visit <a target='_blank' href="http://www.layer7tech.com/">http://www.layer7tech.com/</a>.
+				</div>
+		    </div>
+		    
+		     <?php } ?>
+		    <?php if (0) {?>
+		    <a name="speakers"></a>
+		    <div class="row-fluid">
+				<div class="span2 title_section">
+					SPEAKERS
+				</div>
+				<div class="span10">
+				</div>
+		    </div>
+		    <br/>
+		    <div class="row-fluid">
+				<div class="span2">
+					<img src="img/avatar_defaut.jpg" class="img-rounded">
+				</div>
+				<div class="span10">
+					<a target='_blank' href="http://twitter.com/zubairov">Renat Zubairov</a> is a co-founder of <a target='_blank' href="http://elastic.io/">elastic.io</a>, experienced hacker,
+product owner and agile evangelist. Renat is passionate about SaaS,
+Application Integration and APIs. He is a speaker on international
+conferences, user groups and active open source community member.
+Before elastic.io he was working as Product Owner at Talend, Sopera
+GmbH and Nokia.
+				</div>
+		    </div>
+		    <br/>
+		      <div class="row-fluid">
+				<div class="span2">
+					<a target='_blank' href="http://twitter.com/kinlane"><img src="http://m4.licdn.com/mpr/mpr/shrink_150_150/p/4/000/15a/274/342a544.jpg" class="img-rounded"></a>
+				</div>
+				<div class="span10">
+					<a target='_blank' href="http://twitter.com/kinlane">Kin Lane</a> is a technology professional with an obsession for APIs. He is a renowned blogger in the API sphere: <a target='_blank' href="http://apivoice.com">apivoice.com</a> and <a target='_blank' href="http://apievangelist.com">apievangelist.com</a>.
+				</div>
+		    </div>
+		    <br/>
+		      <div class="row-fluid">
+				<div class="span2">
+					<img src="img/avatar_defaut.jpg" class="img-rounded">
+				</div>
+				<div class="span10">
+					<a target='_blank'  href="https://twitter.com/jakubnesetril">Jakub Nesetril</a> is the founder of <a target='_blank' href="http://apiary.io/">Apiary</a> : a new hot startup building REST API infrastructure for the rest of us. Prior to Apiary he built and led engineering team at GoodData, built their API strategy, presented at conferences around the world about on topics ranging from node.js & javascript to building startups.
+				</div>
+		    </div>
+		    <br/>
+		      <div class="row-fluid">
+				<div class="span2">
+					<img src="img/avatar_defaut.jpg" class="img-rounded">
+				</div>
+				<div class="span10">
+					 David Thoumas - <a href="http://www.opendatasoft.com/">OpenDataSoft</a> co-founder & CTO - 
+David drives OpenDataSoft platform strategy for providing companies with a full-featured cloud-based, data-centric & API-oriented platform. David formerly headed Exalead engineering (a Dassault Systemes Company) and has a great experience of big data issues in industry. OpenDataSoft is dedicated to turn its customers data into API for reaching new business. "Open the data & make them API together with OpenDataSoft, the data&software company".
+				</div>
+		    </div>
+
+		   
+		    <?php } ?>
+		    <?php if ($_GET['do'] == 2) {?>
+		      <br/>
+		      <div class="row-fluid">
+				<div class="span2">
+					<img src="img/avatar_defaut.jpg" class="img-rounded">
+				</div>
+				<div class="span10">
+					John Musser is the founder of ProgrammableWeb.com, the online resource for mashups, APIs and the web as platform. He is a Seattle-based technology consultant, writer and teacher.
+				</div>
+		    </div>
+		     <br/>
+		      <div class="row-fluid">
+				<div class="span2">
+					<img src="img/avatar_defaut.jpg" class="img-rounded">
+				</div>
+				<div class="span10">
+					Steve is a Ruby Hero, software craftsman, and an aspiring digital humanities scholar. He spends most of his time contributing to Open Source projects, and maintains both Hackety Hack andShoes. He also teaches and helps develop curriculum for Jumpstart Lab and has written book on Designing Hypermedia APIs.
+				</div>
+		    </div>
+		      
+		     <?php } ?>
+			
+	    </div>
+	</div>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function () {
+			$('#mce-EMAIL').focus(function() {
+			 	$('#mce-EMAIL').val('');
+			});
+		})
+	</script>
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=223599037652849";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+	</script>
+  </body>
+</html>
